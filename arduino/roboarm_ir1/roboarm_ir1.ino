84579f9dc668f2906916043d4467d2277756fb2a
Dima Sabanin <dsabanin@TATIANAs-MacBook-Pro.local>
2016-08-27 12:34:15 -0400

    more motors added, 3 h-bridges

--- arduino/roboarm_ir1/roboarm_ir1.ino
+++ arduino/roboarm_ir1/roboarm_ir1.ino
@@ -1,19 +1,19 @@
 #include <IRremote.h>
-
-#define MOTOR1_ENABLE 8
-#define MOTOR1_OUT1 9
-#define MOTOR1_OUT2 10
-#define MOTOR2_ENABLE 11
-#define MOTOR2_OUT1 12
-#define MOTOR2_OUT2 13
 #define DEBUG_LED 13
+#define HEAD_LED 12
 
 #define UP_SIGNAL 0x20DFA25D
 #define DOWN_SIGNAL 0x20DF629D
 #define LEFT_SIGNAL 0x20DFE21D
 #define RIGHT_SIGNAL 0x20DF12ED
 #define REPEAT_SIGNAL 0xffffffff
-
+#define VUP_SIGNAL 0x20DF40BF
+#define VDOWN_SIGNAL 0x20DFC03F
+#define CUP_SIGNAL 0x20DF00FF
+#define CDOWN_SIGNAL 0x20DF807F
+#define MUTE_SIGNAL 0x20DF906F
+#define HEAD_SIGNAL 0x20DF58A7
+#define VISIO_SIGNAL 0x20DFB44B
 #define MOVE_STEP 200
 
 typedef enum {UP, DOWN} direction;
@@ -24,10 +24,14 @@ struct motor {
   int out2;
 };
 
-motor m1 = {8, 9, 10};
-motor m2 = {11, 12, 13};
+motor m1 = {A0, A1, A2};
+motor m2 = {A3, A4, A5};
+motor m3 = {2, 3, 4};
+motor m4 = {5, 6, 7};
+motor m5 = {8, 9, 10};
+int headLed = LOW;
 
-IRrecv irrecv(7);
+IRrecv irrecv(11);
 decode_results signals;
 
 void setupMotor(motor m) {
@@ -42,6 +46,9 @@ void setupMotor(motor m) {
 void setup() {
   setupMotor(m1);
   setupMotor(m2);
+  setupMotor(m3);
+  setupMotor(m4);
+  setupMotor(m5);
   Serial.begin(9600);
   pinMode(DEBUG_LED, OUTPUT);
   irrecv.enableIRIn();
@@ -77,9 +84,32 @@ void process_signal(unsigned long cur_signal) {
     case RIGHT_SIGNAL:
       simpleMove(m1, DOWN, MOVE_STEP);
       break;
+    case VUP_SIGNAL:
+      simpleMove(m3, UP, MOVE_STEP);
+      break;
+    case VDOWN_SIGNAL:
+      simpleMove(m3, DOWN, MOVE_STEP);
+      break;
+    case CUP_SIGNAL:
+      simpleMove(m4, UP, MOVE_STEP);
+      break;
+    case CDOWN_SIGNAL:
+      simpleMove(m4, DOWN, MOVE_STEP);
+      break;
+    case MUTE_SIGNAL:
+      simpleMove(m5, UP, MOVE_STEP);
+      break;
+    case HEAD_SIGNAL:
+      simpleMove(m5, DOWN, MOVE_STEP);
+      break;
+    case VISIO_SIGNAL:
+      headLed = headLed == HIGH ? LOW : HIGH;
+      break;
   }
 }
 void loop() {
+  digitalWrite(HEAD_LED, headLed);
+//  digitalWrite(DEBUG_LED, HIGH);
   if (irrecv.decode(&signals)) {
     Serial.println(signals.value, HEX);
     if(previous_signal > 0 && signals.value == REPEAT_SIGNAL) {
@@ -91,4 +121,5 @@ void loop() {
     irrecv.resume();
   }
   delay(20);
+//  digitalWrite(DEBUG_LED, LOW);
 }
